f5a57b3b6757b52c0b72ae90e4a56be04ffcf480
Kyushu <flareonverm@outlook.com>
2022-02-09 22:36:03 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const app = express();
 
 
 app.set("port",8080)
+app.listen(8080)
 
-app.get('/user', (req, res) => {
+app.get('/', (req, res) => {
     console.log("hello")
     res.send("Hello")
 })
